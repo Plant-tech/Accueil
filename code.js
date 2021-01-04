84388944a565fc7e02f2cf9804da248f2d205c4d
leoniegtt <76210870+leoniegtt@users.noreply.github.com>
2021-01-04 16:06:05 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 //création de la 2e liste  pour choisir la couleur de l'arrosoir après avoir choisi le volume
 var colorsBySize = {
-	A: ["Chrome", "Cuivre"],
-	B: ["Chrome", "Cuivre"],
-	C: ["Chrome"]
+	F: ["Chrome", "Cuivre"],
+	G: ["Chrome", "Cuivre"],
+	H: ["Chrome"]
 }
 
 function changeSize(value) {
@@ -34,4 +34,17 @@ function changeSizeP(value) {
 		}
 		document.getElementById("couleurP").innerHTML = sizeOption;
 	}
+}
+
+function ComputePrice() {
+	if (value == A)	prix.value = 10€ ;
+	else if (value == B) prix.value = 15€ ;
+	else if (value == C) prix.value = 23€ ;
+	else if (value == D) prix.value = 30€ ;
+	else if (value == E) prix.value = 40€ ;
+	else if (value == F) prix.value = 20€ ;
+	else if (value == G) prix.value = 25€ ;
+	else if (value == H) prix.value = 30€ ;
+	else if (value == I) prix.value = 15€ ;
+	else prix.value = 100€;
 }
