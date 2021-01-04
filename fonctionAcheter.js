cef71b86b09c0878a81b4bb1fb75ee8783369751
marietousarr <76689981+marietousarr@users.noreply.github.com>
2021-01-04 16:43:25 +0100

    Update fonctionAcheter.js

--- fonctionAcheter.js
+++ fonctionAcheter.js
@@ -1,7 +1,40 @@
 
 
+const panier=document.getElementById("panier");
 
+function AjouterPanier(nomProd) {
 
-function Acheter(nomProd) {
+      const row = document.createElement("tr");
+      const ProdCell = document.createElement("td");
+      const quantityCell = document.createElement("td");
+      const subprice = document.createElement("td");
+      row.className="ligneProduit";
+      subprice.className="soustotal";
+      subprice.innerText= prix;
       
+      // pour la case ProdCell
+      
+      // pour la case de quantité
+      const q= document.createElement("input");
+      q.className="quantity";
+      q.type="number";
+      q.value="1";
+      q.min="1";
+      quantityCell.appendChild(q);
+      
+      const removeButton=document.createElement("button");
+      removeButton.className="bonton boutonblanc";
+      removeButton.id="removeBouton";
+      removeButton.onclick="remove()";
+      removeButton.type="button";
+      removeButton.innerText="Remove";
+      quantityCell.appendChild(removeButton);
+      
+      //pour la case subprice
+      
+      
+      row.appendChild(ProdCell);
+      row.appendChild(quantityCell);
+      row.appendChild(subpriceCell);
+      panier.appendChild(row);
 }
