3ecedde2b3e6a8923a90c59e2869968e7af2c32c
marietousarr <76689981+marietousarr@users.noreply.github.com>
2021-01-09 16:04:05 +0100

    Update Panier.js

--- Panier.js
+++ Panier.js
@@ -167,9 +167,9 @@ function AcheterA(e){
 
 function AcheterM(e){
       alert("le produit va etre ajouté");
-      const couleurchoisie=document.getElementById("couleurM").innerText; 
+      const couleurchoisie=document.getElementById("couleurM").value; 
       const taillechoisie= "Taille unique"; 
-      const prixProdU=document.getElementById("prixM").innerText;
+      const prixProdU=document.getElementById("prixM").value;
       const nomProd ="Musicaplante";
       AjouterPanier(nomProd,couleurchoisie,taillechoisie,prixProdU);
 }
